10fcaefd25f9556ce2ceb913cd8842fe77f955be
Tu Nombre <tu-email@dominio.com>
2025-08-19 10:15:31 -0400

    chore(api): módulos base (auth/usuarios/empresa/suscripcion) + swagger ok

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,14 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth.controller';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { UsuariosController } from './usuarios.controller';
+import { EmpresaModule } from './empresa/empresa.module';
+import { EmpresaController } from './empresa.controller';
+import { SuscripcionModule } from './suscripcion/suscripcion.module';
+import { SuscripcionController } from './suscripcion.controller';
 
 @Module({
   imports: [
     // Carga variables desde .env y las expone en todo el proyecto
     ConfigModule.forRoot({ isGlobal: true }),
     HealthModule,
+    AuthModule,
+    UsuariosModule,
+    EmpresaModule,
+    SuscripcionModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, AuthController, UsuariosController, EmpresaController, SuscripcionController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ api/src/suscripcion.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('suscripcion')
+export class SuscripcionController {}
